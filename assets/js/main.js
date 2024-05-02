9353d373941f97982cf3c732bffa8b7e120de09b
karlrouhana <karlrouhana58@gmail.com>
2024-05-02 19:49:07 +0300

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,7 +92,7 @@
   })
 
   /**
-   * Scrool with ofset on links with a class name .scrollto
+   * ScrolL with ofFset on links with a class name .scrollto
    */
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
@@ -207,35 +207,7 @@
     }
   });
 
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
-
+  
   /**
    * Animation on scroll
    */
